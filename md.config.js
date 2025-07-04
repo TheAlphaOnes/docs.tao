4a67fb176a12895dd785af5b3da02dc9aad86284
akshat227 <akshatkhare364@gmail.com>
2025-07-04 23:01:17 +0530

    update

--- md.config.js
+++ md.config.js
@@ -8,8 +8,32 @@ const MarkdownConfig = {
           link: "cook-intro",
           file: "cook/cook-intro.md",
           type: "tool",
-          sub:  "introduction to cook dev tool"
-        }
+          sub: "introduction to cook dev tool",
+        },
+        {
+          name: "cook-line-of-cod",
+          file: "cook/cook-line-of-code.md",
+          type: "command",
+          sub: "Number of lines of code",
+        },
+        {
+          name: "cook-mold",
+          file: "cook/cook-mold.md",
+          type: "command",
+          sub: "mold",
+        },
+        {
+          name: "cook-tree",
+          file: "cook/cook-tree.md",
+          type: "command",
+          sub: "tree",
+        },
+        {
+          name: "cook-license",
+          file: "cook/license.md",
+          type: "command",
+          sub: "lisensing",
+        },
       ],
     },
     {
@@ -19,15 +43,15 @@ const MarkdownConfig = {
           name: "Intro to aura",
           link: "aura-intro",
           file: "aura/aura-intro.md",
-          type:"service",
-          sub:"introduction to aura ai"
+          type: "service",
+          sub: "introduction to aura ai",
         },
         {
           name: "customize aura",
           link: "aura-customize",
           file: "aura/aura-customize.md",
-          type:"service",
-          sub: "customize the aura ai personality"
+          type: "service",
+          sub: "customize the aura ai personality",
         },
       ],
     },
